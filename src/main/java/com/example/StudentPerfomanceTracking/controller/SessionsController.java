929efa9e8c7c15cc6abfd7d7716b55ef46d5292c
Giaanh0509 <ngotthe0509@gmail.com>
2024-11-16 16:39:48 +0700

    update register

--- src/main/java/com/example/StudentPerfomanceTracking/controller/SessionsController.java
+++ src/main/java/com/example/StudentPerfomanceTracking/controller/SessionsController.java
@@ -5,11 +5,9 @@ import com.example.StudentPerfomanceTracking.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/sessions")
 public class SessionsController {

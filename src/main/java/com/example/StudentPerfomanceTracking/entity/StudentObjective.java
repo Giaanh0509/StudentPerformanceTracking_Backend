9ee891e6245084961024f6a4decb6031bc37e733
Giaanh0509 <ngotthe0509@gmail.com>
2025-02-03 02:05:23 +0700

    update

---
+++ src/main/java/com/example/StudentPerfomanceTracking/entity/StudentObjective.java
@@ -0,0 +1,52 @@
+package com.example.StudentPerfomanceTracking.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students_objectives")
+public class StudentObjective {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "objective_id", referencedColumnName = "id")
+    private Objective objective;
+
+    public StudentObjective() {
+    }
+
+    public StudentObjective(Student student, Objective objective) {
+        this.student = student;
+        this.objective = objective;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Objective getObjective() {
+        return objective;
+    }
+
+    public void setObjective(Objective objective) {
+        this.objective = objective;
+    }
+}
